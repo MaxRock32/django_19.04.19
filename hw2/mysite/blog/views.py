09ea23d9c10c32422e7e0fe05a0392ea3d9c3233
Maxim <maxrock32@bk.ru>
2019-04-25 23:20:40 +0300

    Попробывал сделать что-то новое, а не просто повторить всё за вами (так интересней), старался делать по теме урока и задач. Если, что не так, готов исправить)

---
+++ hw2/mysite/blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+news = [
+    {
+        "title": "Наша первая запись",
+        "text": "Просто большой текст для 1 записи",
+        "date": "1 января 2019",
+        "avtor": "Георгий"
+    },
+    {
+        "title": "Наша вторая запись",
+        "text": "Просто большой текст для 2 записи",
+        "date": "10 января 2019",
+    },
+]
+
+def home(request):
+    data = {
+        "news": news,
+        "title": "Главная страница блога"
+    }
+    return render(request, "blog/home.html", data)
+
+def contacti(request):
+    return render(request, "blog/contacti.html", {"title": "Страничка про нас"})
